ab5c2cbdd1efafbab974a002d1fc9d369ddcb8b6
Hub123 <nachson1992@gmail.com>
2022-07-28 14:21:06 +0300

    sql

--- index.sql
+++ index.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA job_placements;
 
 CREATE FUNCTION trigger_set_timestamp()
-RETURN TRIGGER AS $$
+RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = NOW();
     RETURN NEW;
@@ -9,64 +9,64 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TABLE job_placements.jobs (
-    id PRIMARY KEY SERIAL,
+    id SERIAL PRIMARY KEY,
     created_at DATE NOT NULL DEFAULT NOW(),
     updated_at DATE NOT NULL DEFAULT NOW(),
-    is_deleted BOOLEAN NOT NULL DEAFULT FALSE,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
 
     title VARCHAR(255) NOT NULL
-)
+);
 
-CREATE TRIGGER job_placements.set_timestamp
+CREATE TRIGGER set_timestamp_jobs
 BEFORE UPDATE
 ON job_placements.jobs
 FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp;
+EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TABLE job_placements.roles (
-    id PRIMARY KEY SERIAL,
+    id SERIAL PRIMARY KEY,
     created_at DATE NOT NULL DEFAULT NOW(),
     updated_at DATE NOT NULL DEFAULT NOW(),
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
 
     title VARCHAR(255) NOT NULL
-)
+);
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_roles
 BEFORE UPDATE
 ON job_placements.roles
 FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp;
+EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TABLE job_placements.job_roles (
-    id PRIMARY KEY SERIAL,
+    id SERIAL PRIMARY KEY,
     created_at DATE NOT NULL DEFAULT NOW(),
     updated_at DATE NOT NULL DEFAULT NOW(),
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
 
-    job_id FOREIGN KEY NOT NULL,
-    role_id FOREIGN KEY NOT NULL
-)
+    CONSTRAINT job_id FOREIGN KEY(id) REFERENCES jobs(id),
+    CONSTRAINT role_id FOREIGN KEY(id) REFERENCES roles(id)
+);
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_job_roles
 BEFORE UPDATE
 ON job_placements.job_roles
 FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp;
+EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TABLE job_placements.mannings (
-    id PRIMARY KEY SERIAL,
+    id SERIAL PRIMARY KEY,
     created_at DATE NOT NULL DEFAULT NOW(),
     updated_at DATE NOT NULL DEFAULT NOW(),
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
 
     name VARCHAR(255),
     start_date DATE NOT NULL,
-    job_role_id FOREIGN KEY NOT NULL
-)
+    CONSTRAINT job_role_id FOREIGN KEY (id) REFERENCES job_placements.job_roles(id)
+);
 
-CREATE TRIGGER set_timestamp
+CREATE TRIGGER set_timestamp_mannings
 BEFORE UPDATE
 ON job_placements.mannings
 FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp;
+EXECUTE PROCEDURE trigger_set_timestamp();
